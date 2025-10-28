4e6f7c4831b7c2d09c1f980be00749272c47fd60
Om-mac <omtapdiya75@gmail.com>
2025-10-28 21:12:37 +0530

    added update functionality

--- flask_app/app.py
+++ flask_app/app.py
@@ -15,35 +15,35 @@ sensor_data = {
 
 door_command = "CLOSED"
 
-# === Twilio WhatsApp Configuration ===
+
 account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
 auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 
-TWILIO_WHATSAPP_FROM = "whatsapp:+14155238886"   # Twilio sandbox number
-TO_WHATSAPP_NUMBER = "whatsapp:+919420440361"    # Your verified WhatsApp number
+TWILIO_WHATSAPP_FROM = "whatsapp:+14155238886"  
+TO_WHATSAPP_NUMBER = "whatsapp:+919420440361"    
 
 
-#dashboard
+
 @app.route('/')
 def index():
     return render_template('index.html', data=sensor_data)
 
 
-# ESP32 updates sensor values 
+
 @app.route('/update')
 def update():
     global sensor_data
 
-    # Get sensor readings from ESP32
+
     sensor_data['temp'] = request.args.get('temp', 0)
     sensor_data['hum'] = request.args.get('hum', 0)
     sensor_data['mq2'] = request.args.get('mq2', 0)
     sensor_data['mq6'] = request.args.get('mq6', 0)
     sensor_data['flame'] = int(request.args.get('flame', 1))
 
-    # WhatsApp Alert if Flame Detected
-    if sensor_data['flame'] == 0:   # 0 means flame is  detected (as flame sensor o/p)
+
+    if sensor_data['flame'] == 0:  
         try:
             message = client.messages.create(
                 from_=TWILIO_WHATSAPP_FROM,
@@ -57,13 +57,13 @@ def update():
     return "OK"
 
 
-# === Route for the dashboard JS to fetch live data ===
+
 @app.route('/data')
 def data():
     return jsonify(sensor_data)
 
 
-# === Webpage button triggers door open ===
+
 @app.route('/open-door')
 def open_door():
     global door_command
@@ -71,13 +71,13 @@ def open_door():
     return redirect("/")
 
 
-# === ESP32 checks this to see if door should open ===
+
 @app.route('/door_status')
 def door_status():
     return door_command
 
 
-# === ESP32 resets door command after opening ===
+
 @app.route('/door_status_reset')
 def door_status_reset():
     global door_command
@@ -86,5 +86,5 @@ def door_status_reset():
 
 
 if __name__ == "__main__":
-    # host="0.0.0.0" allows ESP32 and other LAN devices to connect
+
     app.run(host="0.0.0.0", port=5000, debug=True)
